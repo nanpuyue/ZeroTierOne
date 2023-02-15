97aa3f83963f3143439d4f0dc65eaca9568eec6f
南浦月 <nanpuyue@gmail.com>
2023-02-15 23:00:33 +0800

    feat: remove debug code

--- osdep/LinuxEthernetTap.cpp
+++ osdep/LinuxEthernetTap.cpp
@@ -52,9 +52,6 @@
 #include <algorithm>
 #include <utility>
 #include <string>
-#include <iostream>
-#include <iomanip>
-#include <cctype>
 
 #include <ctype.h>
 #include <sys/utsname.h>
@@ -113,66 +110,11 @@ static void _base32_5_to_8(const uint8_t *in,char *out)
 	out[7] = _base32_chars[(in[4] & 0x1f)];
 }
 
-std::ostream& render_printable_chars(std::ostream& os, const char* buffer, size_t bufsize) {
-	os << " | ";
-	for (size_t i = 0; i < bufsize; ++i) {
-		if (std::isprint(buffer[i])) {
-			os << buffer[i];
-		} else {
-			os << ".";
-		}
-	}
-	return os;
-}
-
-std::ostream& hex_dump(std::ostream& os, const uint8_t* buffer, size_t bufsize, bool showPrintableChars = true) {
-	auto oldFormat = os.flags();
-	auto oldFillChar = os.fill();
-
-	os << std::hex;
-	os.fill('0');
-	bool printBlank = false;
-	size_t i = 0;
-	for (; i < bufsize; ++i) {
-		if (i % 8 == 0) {
-			if (i != 0 && showPrintableChars) {
-				render_printable_chars(os, reinterpret_cast<const char*>(&buffer[i] - 8), 8);
-			}
-			os << std::endl;
-			printBlank = false;
-		}
-		if (printBlank) {
-			os << ' ';
-		}
-		os << std::setw(2) << std::right << unsigned(buffer[i]);
-		if (!printBlank) {
-			printBlank = true;
-		}
-	}
-	if (i % 8 != 0 && showPrintableChars) {
-		for (size_t j = 0; j < 8 - (i % 8); ++j) {
-			os << "   ";
-		}
-		render_printable_chars(os, reinterpret_cast<const char*>(&buffer[i] - (i % 8)), (i % 8));
-	}
-
-	os << std::endl;
-
-	os.fill(oldFillChar);
-	os.flags(oldFormat);
-
-	return os;
-}
-
 void LinuxEthernetTap::_pcap_cb(u_char *user, const pcap_pkthdr *h, const u_char *packet){
 	class LinuxEthernetTap* cb_user = (class LinuxEthernetTap*)user;
 	unsigned int len = h->caplen;
-
-	// hex_dump(std::cout, (const uint8_t *)(packet),(unsigned int)(len));
 	unsigned int etherType = Utils::ntoh(((const uint16_t *)packet)[6]);
 	MAC to(packet, 6),from(packet + 6, 6);
-	// char buf[18];
-	// std::cout << from.toString(buf) << " -> " << to.toString(buf) << std::endl;
 	cb_user->_handler(cb_user->_arg, nullptr, cb_user->_nwid, from, to, etherType, 0, (const void *)(packet + 14),(unsigned int)(len - 14));
 }
 
